4c77f21f78800ad845acf9e298b49f08ae790801
Tchoko Apps <tchokoapps@gmail.com>
2018-06-03 12:17:57 +0200

    commit 001

---
+++ src/main/java/com/tchoko/springboot/coffeshopapp/SpringBootCoffeeShopAppApplication.java
@@ -0,0 +1,12 @@
+package com.tchoko.springboot.coffeshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoffeeShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoffeeShopAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tchoko/springboot/coffeshopapp/SpringBootCoffeeShopAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tchoko.springboot.coffeshopapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCoffeeShopAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
